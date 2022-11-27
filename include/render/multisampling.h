6cc59f334c0d3a4772d4cff9255d019f9220e066
Jed <jedfakhfekh@gmail.com>
2022-11-28 03:54:28 +0900

    Finish vulkan init migration and start cleaning

--- include/render/multisampling.h
+++ include/render/multisampling.h
@@ -1,26 +1,5 @@
-//#define GLFW_INCLUDE_VULKAN
-//#include <GLFW/glfw3.h>
-//
-//
-//
-//
-//
-//#define TINYOBJLOADER_IMPLEMENTATION
-//#include <tiny_obj_loader.h>
 #pragma once
 #include "render/vulkan_core.h"
-
-////IMAGE
-//#ifndef STB_IMAGE_IMPLEMENTATION
-//#define STB_IMAGE_IMPLEMENTATION
-//#include <stb_image.h>
-//#endif //STB_IMAGE_IMPLEMENTATION
-//
-////TINYOBJ
-//#ifndef TINYOBJLOADER_IMPLEMENTATION
-//#define TINYOBJLOADER_IMPLEMENTATION
-//#include <tiny_obj_loader.h>
-//#endif // TINYOBJLOADER_IMPLEMENTATION
 
 
 #include <iostream>
@@ -236,12 +215,12 @@ private:
 
     void initVulkan() {
         as::vulkan_instance_create_info instance_create_info = {};
-        as::create_vulkan_instance(instance, instance_create_info); //createInstance();
+        as::create_vulkan_instance(instance, instance_create_info);
         if (instance_create_info.enable_validation_layers)
         {
-			as::setup_debug_messenger(&instance, &debugMessenger); //setupDebugMessenger();
+			as::setup_debug_messenger(&instance, &debugMessenger);
         }
-		as::create_surface(surface, instance, window);// createSurface();
+		as::create_surface(surface, instance, window);
         as::pick_physical_device(&physicalDevice, &msaaSamples, &instance, &surface); //pickPhysicalDevice();
         as::create_logical_device(&device, &graphicsQueue, &presentQueue, &physicalDevice, &surface, deviceExtensions, validationLayers); //createLogicalDevice();
         as::create_swap_chain(&swapChain, &swapChainImages, &swapChainImageFormat, &swapChainExtent, &device, &physicalDevice, &surface, window); //createSwapChain();
@@ -264,7 +243,7 @@ private:
         as::create_descriptor_sets(descriptorSets, device, descriptorSetLayout, descriptorPool, MAX_FRAMES_IN_FLIGHT); //createDescriptorSets();
         as::update_descriptor_sets(device, descriptorSets, uniformBuffers, MAX_FRAMES_IN_FLIGHT, textureImageView, textureSampler);
         as::create_command_buffers(commandBuffers, device, commandPool, MAX_FRAMES_IN_FLIGHT); //createCommandBuffers();
-        createSyncObjects();
+        as::create_sync_objects(device, imageAvailableSemaphores, renderFinishedSemaphores, inFlightFences, MAX_FRAMES_IN_FLIGHT); //createSyncObjects();
     }
 
     void mainLoop() {
@@ -365,92 +344,12 @@ private:
         createFramebuffers();
     }
 
-    void createInstance() {
-        if (enableValidationLayers && !checkValidationLayerSupport()) {
-            throw std::runtime_error("validation layers requested, but not available!");
-        }
-
-        VkApplicationInfo appInfo{};
-        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        appInfo.pApplicationName = "Hello Triangle";
-        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.pEngineName = "No Engine";
-        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.apiVersion = VK_API_VERSION_1_0;
-
-        VkInstanceCreateInfo createInfo{};
-        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-        createInfo.pApplicationInfo = &appInfo;
-
-        auto extensions = getRequiredExtensions();
-        createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
-        createInfo.ppEnabledExtensionNames = extensions.data();
-
-        VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
-        if (enableValidationLayers) {
-            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
-            createInfo.ppEnabledLayerNames = validationLayers.data();
-
-            populateDebugMessengerCreateInfo(debugCreateInfo);
-            createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*) &debugCreateInfo;
-        } else {
-            createInfo.enabledLayerCount = 0;
-
-            createInfo.pNext = nullptr;
-        }
-
-        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
-            throw std::runtime_error("failed to create instance!");
-        }
-    }
-
     void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
         createInfo = {};
         createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
         createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
         createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
         createInfo.pfnUserCallback = debugCallback;
-    }
-
-    void setupDebugMessenger() {
-        if (!enableValidationLayers) return;
-
-        VkDebugUtilsMessengerCreateInfoEXT createInfo;
-        populateDebugMessengerCreateInfo(createInfo);
-
-        if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS) {
-            throw std::runtime_error("failed to set up debug messenger!");
-        }
-    }
-
-    void createSurface() {
-        if (glfwCreateWindowSurface(instance, window, nullptr, &surface) != VK_SUCCESS) {
-            throw std::runtime_error("failed to create window surface!");
-        }
-    }
-
-    void pickPhysicalDevice() {
-        uint32_t deviceCount = 0;
-        vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
-
-        if (deviceCount == 0) {
-            throw std::runtime_error("failed to find GPUs with Vulkan support!");
-        }
-
-        std::vector<VkPhysicalDevice> devices(deviceCount);
-        vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
-
-        for (const auto& device : devices) {
-            if (isDeviceSuitable(device)) {
-                physicalDevice = device;
-                msaaSamples = getMaxUsableSampleCount();
-                break;
-            }
-        }
-
-        if (physicalDevice == VK_NULL_HANDLE) {
-            throw std::runtime_error("failed to find a suitable GPU!");
-        }
     }
 
     void createLogicalDevice() {
