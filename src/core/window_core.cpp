950e9de9f078c5179be11e5920acff6f72875bcc
オウギワシ <jedfakhfekh@gmail.com>
2022-12-31 13:53:00 +0900

    Changed linux framebuffer writing implementation

--- src/core/window_core.cpp
+++ src/core/window_core.cpp
@@ -1,12 +1,13 @@
 #if defined(_WIN32)
 #include <windows.h>
 #else
-#include <linux/fb.h>
-#include <time.h>
-#include <sys/stat.h>
-#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 #include <fcntl.h>
+#include <linux/fb.h>
 #include <sys/mman.h>
+#include <sys/ioctl.h>
 #endif
 
 //#include "opengl/opengl.h"
@@ -65,68 +66,74 @@
 
 	bool as::create_window(as::window& window, const u16& height, const u16& width)
 	{
-		struct fb_fix_screeninfo finfo;
+		int fbfd = 0;
 		struct fb_var_screeninfo vinfo;
+		struct fb_fix_screeninfo finfo;
+		long int screensize = 0;
+		char* fbp = 0;
+		int x = 0, y = 0;
+		long int location = 0;
 
-		i32 fb_fd = open("/dev/fb0", O_RDWR);
+		// Open the file for reading and writing
+		fbfd = open("/dev/fb0", O_RDWR);
+		if (fbfd == -1) {
+			perror("Error: cannot open framebuffer device");
+			exit(1);
+		}
+		printf("The framebuffer device was opened successfully.\n");
 
-		// set grayscale 0 and bitperpixel to 32 (default 8)
-		{
-			//Get variable screen information
-			ioctl(fb_fd, FBIOGET_VSCREENINFO, &vinfo);
-			vinfo.grayscale = 0;
-			vinfo.bits_per_pixel = 32;
-			ioctl(fb_fd, FBIOPUT_VSCREENINFO, &vinfo);
-			ioctl(fb_fd, FBIOGET_VSCREENINFO, &vinfo);
-			ioctl(fb_fd, FBIOGET_FSCREENINFO, &finfo);
-			AS_LOG(LV_LOG, "Changed bit-per-pixel to 32");
+		// Get fixed screen information
+		if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo) == -1) {
+			perror("Error reading fixed information");
+			exit(2);
 		}
 
-		// screen
-		{
+		// Get variable screen information
+		if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo) == -1) {
+			perror("Error reading variable information");
+			exit(3);
+		}
 
-			// screen size
-			i64 screensize = vinfo.yres_virtual * finfo.line_length;
+		printf("%dx%d, %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel);
 
-			if (screensize > 0)
-			{
-				AS_LOG(LV_LOG, "Screen size is " + std::to_string(screensize));
-			}
-			else
-			{
+		// Figure out the size of the screen in bytes
+		screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
 
-				AS_LOG(LV_WARNING, "Screen size is " + std::to_string(screensize) + " cannot create window");
-				return false;	
-			}
-			// map pixels to memory buffer
-			u8* fbp = (u8*)(mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd, (off_t)0));
-			if (fbp)
-			{
-				AS_LOG(LV_LOG, "Mapped pixels to memory buffer");
-				i64 x, y; //location we want to draw the pixel
-				u32 pixel = 0x505050; //The pixel we want to draw at that location
+		// Map the device to memory
+		fbp = (char*)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0);
+		if ((i64)fbp == -1) {
+			perror("Error: failed to map framebuffer device to memory");
+			exit(4);
+		}
+		printf("The framebuffer device was mapped to memory successfully.\n");
 
-				//Make sure you set x,y and pixel correctly
-				i64 location = (x + vinfo.xoffset) * (vinfo.bits_per_pixel / 8) + (y + vinfo.yoffset) * finfo.line_length;
-				
-				if ((fbp + location) != nullptr)
-				{
-					AS_LOG(LV_LOG, "Setting pixel data");
-					*(fbp + location) = 0;
-					//*((u32*)(fbp + location)) = pixel;
-					
+		x = 100; y = 100;       // Where we are going to put the pixel
+
+		// Figure out where in memory to put the pixel
+		for (y = 100; y < 300; y++)
+			for (x = 100; x < 300; x++) {
+
+				location = (x + vinfo.xoffset) * (vinfo.bits_per_pixel / 8) +
+					(y + vinfo.yoffset) * finfo.line_length;
+
+				if (vinfo.bits_per_pixel == 32) {
+					*(fbp + location) = 100;        // Some blue
+					*(fbp + location + 1) = 15 + (x - 100) / 2;     // A little green
+					*(fbp + location + 2) = 200 - (y - 100) / 5;    // A lot of red
+					*(fbp + location + 3) = 0;      // No transparency
+					//location += 4;
 				}
-				
+				else { //assume 16bpp
+					int b = 10;
+					int g = (x - 100) / 6;     // A little green
+					int r = 31 - (y - 100) / 16;    // A lot of red
+					unsigned short int t = r << 11 | g << 5 | b;
+					*((unsigned short int*)(fbp + location)) = t;
+				}
 
-				struct timespec rqtp, rmtp = { 3, 500 };
-				nanosleep(&rqtp, &rmtp);
 			}
-			else
-			{
-				AS_LOG(LV_ERROR, "Failed to map the pixels to the frame buffer");
-			}
-			
-		}
+		munmap(fbp, screensize);
+		close(fbfd);
 
 		return false;
 	}
