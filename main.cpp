0c64a535acb5f176530607ae5653f532ed67b9bd
オウギワシ <jedfakhfekh@gmail.com>
2023-01-14 17:12:53 +0900

    Remove nuklear (bad documentation)

--- main.cpp
+++ main.cpp
@@ -2,13 +2,6 @@
 #include "engine_utility.h"
 #include "display_core.h"
 
-#define NK_IMPLEMENTATION
-#include "nuklear/nuklear.h"
-
-#define NK_INCLUDE_DEFAULT_FONT
-#define NK_INCLUDE_FONT_BAKING
-
-#define MAX_MEMORY 512000000
 
 i32 main()
 {
@@ -58,40 +51,6 @@ i32 main()
 	as::timer::handle timer_handle;
 	as::timer::start_timer(timer_handle);
 
-	//// init gui state
-	//enum { EASY, HARD };
-	//static int op = EASY;
-	//static float value = 0.6f;
-	//static int i = 20;
-
-
-
-	//struct nk_context ctx;
-	//struct nk_font_atlas font_atlas;
-	//struct nk_font* font = nk_font_atlas_add_default(&font_atlas, 14, NULL);
-	//
-	//nk_init_fixed(&ctx, calloc(1, MAX_MEMORY), MAX_MEMORY, &font);
-	//if (nk_begin(&ctx, "Show", nk_rect(50, 50, 220, 220),
-	//	NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_CLOSABLE)) {
-	//	// fixed widget pixel width
-	//	nk_layout_row_static(&ctx, 30, 80, 1);
-	//	if (nk_button_label(&ctx, "button")) {
-	//		// event handling
-	//	}
-	//	// fixed widget window ratio width
-	//	nk_layout_row_dynamic(&ctx, 30, 2);
-	//	if (nk_option_label(&ctx, "easy", op == EASY)) op = EASY;
-	//	if (nk_option_label(&ctx, "hard", op == HARD)) op = HARD;
-	//	// custom widget pixel width
-	//	nk_layout_row_begin(&ctx, NK_STATIC, 30, 2);
-	//	{
-	//		nk_layout_row_push(&ctx, 50);
-	//		nk_label(&ctx, "Volume:", NK_TEXT_LEFT);
-	//		nk_layout_row_push(&ctx, 110);
-	//		nk_slider_float(&ctx, 0, &value, 1.0f, 0.1f);
-	//	}
-	//	nk_layout_row_end(&ctx);
-	//}
 
 	while (as::should_display_loop(display_handle))
 	{
