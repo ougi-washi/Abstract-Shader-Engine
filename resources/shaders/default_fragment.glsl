24b52af50fee9ea4aed2df8f0a9255db82c9939f
オウギワシ <jedfakhfekh@gmail.com>
2024-02-12 03:24:03 +0900

    Move fragment layouts to a different folder

--- resources/shaders/default_fragment.glsl
+++ resources/shaders/default_fragment.glsl
@@ -2,49 +2,8 @@
 
 #version 450
 #include "core/as_common.glsl"
+#include "core/as_fragment_layout.glsl"
 #include "as_sdf.glsl"
-
-struct as_light 
-{
-    vec3 position;
-    vec3 color;
-    float radius;
-};
-
-struct as_mat4_array 
-{
-    mat4 data[128];
-    int size;
-};
-
-struct as_lights_array 
-{
-    as_light data[128];
-    int size;
-};
-
-struct as_scene 
-{
-    as_mat4_array objects_transforms;
-    as_lights_array lights;
-};
-
-layout(set = 0, binding = 0) buffer SceneBuffer 
-{
-    as_scene scene;
-};
-
-layout(binding = 1) uniform sampler2D tex_sampler;
-
-// has to be this order to match as_vertex
-layout(location = 0) in vec3 vert_pos;
-layout(location = 1) in vec3 frag_normal;
-layout(location = 2) in vec3 frag_color;
-layout(location = 3) in vec2 frag_tex_coord;
-layout(location = 4) in vec3 obj_position;// instances share the same obj position so they don't work, they have to be adjusted accordingly
-layout(location = 5) in flat int instance_id; 
-
-layout(location = 0) out vec4 out_color;
 
 
 sdf_result sdf_scene(vec3 p)
