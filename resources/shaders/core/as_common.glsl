ec160cef56d2698cd3ce33c1e9e0fc24a3a5a574
オウギワシ <jedfakhfekh@gmail.com>
2024-02-19 05:25:09 +0900

    Fix raymarching transforms to blend between separate objects

--- resources/shaders/core/as_common.glsl
+++ resources/shaders/core/as_common.glsl
@@ -20,14 +20,16 @@ layout(push_constant) uniform push_constant_buffer
 	// mouse_data		X[3][0] Y[3][1]
 } ps;
 
+vec3 get_position(mat4 transform) { return vec3(transform[3][0], transform[3][1], transform[3][2]); }
 vec3 get_camera_pos() { return vec3(ps.data[0][0], ps.data[0][1], ps.data[0][2]); }
 vec3 get_camera_dir() { return vec3(ps.data[1][0], ps.data[1][1], ps.data[1][2]); }
 float get_current_time() { return ps.data[2][0]; }
 int get_object_index() { return int(ps.data[2][1]); }
 vec2 get_mouse_pos() { return vec2(ps.data[3][0], ps.data[3][1]); }
 mat4 get_object_transform(int index) { return ubo.objects_transforms[index]; }
+vec3 get_object_position(int index) { return get_position(get_object_transform(index)); }
 mat4 get_current_object_transform() { return ubo.objects_transforms[get_object_index()]; }
-vec3 get_position(mat4 transform) { return vec3(transform[3][0], transform[3][1], transform[3][2]); }
+vec3 get_current_object_position() { return get_object_position(get_object_index()); }
 int get_object_count() { return int(ubo.scene_info[0][0]); }
 
 mat4 look_at(vec3 eye, vec3 center, vec3 up) 
