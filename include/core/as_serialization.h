0d5c0fa0a179b212729cf872a1bb8569a54a82af
オウギワシ <jedfakhfekh@gmail.com>
2024-02-28 03:46:13 +0900

    Add serialization and deserialization functions

--- include/core/as_serialization.h
+++ include/core/as_serialization.h
@@ -5,34 +5,67 @@
 #include "as_types.h"
 #include "as_math.h"
 #include "core/as_shapes.h"
+#include "defines/as_global.h"
 
-typedef struct as_shader_serialized
+struct as_shader;
+struct as_object;
+struct as_camera;
+struct as_light;
+struct as_scene;
+struct as_render_queue;
+struct as_render;
+
+typedef struct as_serialized_shader
 {
 	char filename_vertex[AS_MAX_PATH_SIZE];
 	char filename_fragment[AS_MAX_PATH_SIZE];
 
 	//struct as_shader_uniforms_32 uniforms; resolve this later
-} as_shader_serialized;
+} as_serialized_shader;
 
-typedef struct as_object_serialized
+typedef struct as_serialized_object
 {
 	as_transform transform;
 	u32 instance_count;
 	as_shape shape;
-	as_shader_serialized shader;
+	as_serialized_shader shader;
 	i32 scene_gpu_index; // index of the object in the GPU scene 
 
-} as_object_serialized;
+} as_serialized_object;
+AS_ARRAY_DECLARE(as_serialized_scene_objects, AS_MAX_SCENE_CAMERAS, as_serialized_object);
 
-typedef struct as_light_serialized
+typedef struct as_serialized_light
 {
 	as_vec3 position;
 	as_vec3 color;
 	f32 radius;
 
-} as_light;
+} as_serialized_light;
+AS_ARRAY_DECLARE(as_serialized_scene_lights, AS_MAX_SCENE_LIGHTS, as_serialized_light);
 
-typedef struct as_scene_serialized 
+typedef struct as_serialized_camera
 {
-	
-} as_scene_serialized;
+	as_vec3 position;
+	as_vec3 color;
+	f32 radius;
+
+} as_serialized_camera;
+AS_ARRAY_DECLARE(as_serialized_scene_cameras, AS_MAX_SCENE_LIGHTS, as_serialized_camera);
+
+typedef struct as_serialized_scene 
+{
+	char path[AS_MAX_PATH_SIZE];
+	as_serialized_scene_objects objects;
+	as_serialized_scene_lights lights;
+	as_serialized_scene_cameras cameras;
+
+} as_serialized_scene;
+
+as_serialized_shader* as_serialize_shader(as_shader* shader);
+as_shader* as_deserialize_shader(as_render_queue* render_queue, as_render* render, as_serialized_shader* serialized_shader);
+
+as_serialized_object* as_serialize_object(as_object* object);
+as_object* as_deserialize_object(as_render_queue* render_queue, as_render* render, as_serialized_object* serialized_object);
+
+as_serialized_scene* as_serialize_scene(as_scene* scene);
+as_scene* as_deserialize_scene(as_render_queue* render_queue, as_render* render, as_serialized_scene* serialized_scene);
--- src/core/as_serialization.c
+++ src/core/as_serialization.c
@@ -1,5 +1,47 @@
 // Abstract Shader Engine - Jed Fakhfekh - https://github.com/ougi-washi
 
 #include "core/as_serialization.h"
+#include "core/as_render_queue.h"
+#include "as_memory.h"
 
+as_serialized_shader* as_serialize_shader(as_shader* shader)
+{
+	AS_ASSERT(shader, "Cannot serialize shader, invalid ptr");
+	return NULL;
+}
 
+as_shader* as_deserialize_shader(as_render* render, as_render_queue* render_queue, as_serialized_shader* serialized_shader)
+{
+	AS_ASSERT(serialized_shader, "Cannot deserialize shader, invalid serialized ptr");
+
+	as_shader* shader = AS_MALLOC_SINGLE(as_shader);
+	
+	strcpy(shader->filename_vertex, serialized_shader->filename_vertex);
+	strcpy(shader->filename_fragment, serialized_shader->filename_fragment);
+	as_rq_shader_update(render_queue, render, shader);
+	return shader;
+}
+
+as_serialized_object* as_serialize_object(as_object* object)
+{
+	AS_ASSERT(object, "Cannot serialize object, invalid ptr");
+	return NULL;
+}
+
+as_object* as_deserialize_object(as_render* render, as_render_queue* render_queue, as_serialized_object* serialized_object)
+{
+	AS_ASSERT(serialized_object, "Cannot deserialize object, invalid serialized ptr");
+	return NULL;
+}
+
+as_serialized_scene* as_serialize_scene(as_scene* scene)
+{
+	AS_ASSERT(scene, "Cannot serialize scene, invalid ptr");
+	return NULL;
+}
+
+as_scene* as_deserialize_scene(as_render* render, as_render_queue* render_queue, as_serialized_scene* serialized_scene)
+{
+	AS_ASSERT(serialized_scene, "Cannot deserialize scene, invalid serialized ptr");
+	return NULL;
+}
