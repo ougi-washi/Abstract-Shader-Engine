4159391ef6450606a41833d4a1a073f8f867f93d
オウギワシ <jedfakhfekh@gmail.com>
2024-03-02 05:49:04 +0900

    Fix color blending weight between objects

--- resources/shaders/default_fragment.glsl
+++ resources/shaders/default_fragment.glsl
@@ -20,28 +20,33 @@ sdf_result sdf_scene(vec3 p)
         vec3 sphere_color = vec3(0.);
         if (i == 0)
         {
-            sphere_dist = sd_sphere(p - get_object_position(0), .6);
-            sphere_color = vec3(1.0, 0., 0.0);
+            sphere_dist = sd_sphere(p - get_object_position(i), .6);
+            sphere_color =  vec3(1.0, 0., 0.0);
         }
         else if (i == 1)
         {
-            sphere_dist = sd_sphere(p - get_object_position(1), 0.6);
+            sphere_dist = sd_sphere(p - get_object_position(i), 0.6);
             sphere_color =   vec3(0., 1., 0.);
         }   
-
+        else if (i == 2)
+        {
+            sphere_dist = sd_sphere(p - get_object_position(i), 0.3);
+            sphere_color =   vec3(0., .0, 1.);
+        }
+        float color_weight = 1. - smoothstep(0.0, 0.5, sphere_dist);
         blended_dist = op_smooth_union(blended_dist, sphere_dist, 1.);
-        blended_color = mix(blended_color, sphere_color, blended_dist);
+        blended_color = mix(blended_color, sphere_color, color_weight);
     }
-
+    
     return sdf_result(p, blended_color, blended_dist);
 }
 
 void main()
 {
-    vec3 world_pos = vert_pos + get_current_object_position() ;
-    vec3 cam_pos = get_camera_pos() - get_current_object_position();
-    vec3 ray_dir = normalize(world_pos - cam_pos);
-    sdf_result sdf = raymarch(cam_pos, ray_dir);
+    const vec3 world_vert_pos = vert_pos + get_current_object_position() ;
+    const vec3 world_cam_pos = get_camera_pos() - get_current_object_position();
+    const vec3 ray_dir = normalize(world_vert_pos - world_cam_pos);
+    const sdf_result sdf = raymarch(world_cam_pos, ray_dir);
     if(sdf.alpha < .4) { discard; }
     out_color = vec4(sdf.color, 1.);
 }
--- src/as_editor.c
+++ src/as_editor.c
@@ -10,7 +10,12 @@ void as_rotate_object1(as_object* object, const f64 delta_time)
 
 void as_rotate_object2(as_object* object, const f64 delta_time)
 {
-	as_object_set_translation(object, AS_VEC_PTR(as_vec3, 0., sin(as_get_time() * .3) * 2.));
+	as_object_set_translation(object, AS_VEC_PTR(as_vec3, 0., sin(as_get_time() * 3.) * 2.));
+}
+
+void as_rotate_object3(as_object* object, const f64 delta_time)
+{
+	as_object_set_translation(object, AS_VEC_PTR(as_vec3, 0., cos(as_get_time() * 2.) * 3.));
 }
 
 void as_editor_set_default_scene()
@@ -33,18 +38,19 @@ void as_editor_set_default_scene()
 
 	as_object* object2 = as_object_create_with_tick(shape_sphere, shader, &as_rotate_object2);
 	as_object_set_translation(object2, AS_VEC_PTR(as_vec3, 0., -1.3, 0.));
-	as_object_set_instance_count(object2, 1);
+
+	as_object* object3 = as_object_create_with_tick(shape_sphere, shader, &as_rotate_object3);
 
 	as_shader* gray_shader = as_shader_create(AS_PATH_DEFAULT_VERT_SHADER, AS_PATH_EMPTY_GRAY_FRAG_SHADER);
 	as_shape* shape_cube = as_generate_cube();
 	as_asset_register(shape_cube, AS_ASSET_TYPE_SHAPE); // registering it as asset to make sure it's cleared when shutting down the engine 
 
-	as_object* object3 = as_object_create(shape_cube, gray_shader);
 	as_object* object4 = as_object_create(shape_cube, gray_shader);
 	as_object* object5 = as_object_create(shape_cube, gray_shader);
-	as_object_set_translation(object3, AS_VEC_PTR(as_vec3, 0., 0., -5.));
-	as_object_set_translation(object4, AS_VEC_PTR(as_vec3, 0., 3., -5.));
-	as_object_set_translation(object5, AS_VEC_PTR(as_vec3, 0., -3., -5.));
+	as_object* object6 = as_object_create(shape_cube, gray_shader);
+	as_object_set_translation(object4, AS_VEC_PTR(as_vec3, 0., 0., -5.));
+	as_object_set_translation(object5, AS_VEC_PTR(as_vec3, 0., 3., -5.));
+	as_object_set_translation(object6, AS_VEC_PTR(as_vec3, 0., -3., -5.));
 }
 
 bool as_editor_should_loop()
