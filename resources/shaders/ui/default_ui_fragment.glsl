8a39c6f6484534eb0c42ca5e1a59df711da2cd0d
オウギワシ <jedfakhfekh@gmail.com>
2024-03-17 23:52:00 +0900

    Add box sdf to the test UI shader

--- resources/shaders/ui/default_ui_fragment.glsl
+++ resources/shaders/ui/default_ui_fragment.glsl
@@ -3,6 +3,7 @@
 #version 450
 
 #include "as_ui_layout.glsl"
+#include "as_ui_sdf_shapes.glsl"
 
 //layout(binding = 1) uniform sampler2D tex_sampler;
 
@@ -14,7 +15,8 @@ void main()
    // out_color = texture(tex_sampler, uv);
     out_color = vec4(1.);
     vec2 center_uv = uv - vec2(.5);
-    float sphere_mask = length(center_uv) - .3f;
+    float sphere_mask = sd_box(uv - .5, vec2(.1f));
+    //float sphere_mask = length(center_uv) - .3f;
     sphere_mask = smoothstep(0.0, .3, sphere_mask);
     out_color = vec4(uv.y * out_color.x, out_color.y, out_color.z, sphere_mask);
 }
