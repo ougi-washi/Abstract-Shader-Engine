f0e611b10df53f27949719fb7db1455264e1bf6b
オウギワシ <jedfakhfekh@gmail.com>
2024-03-26 22:59:02 +0900

    Remove extra commented shader code

--- resources/shaders/core_2d/as_2d_layout.glsl
+++ resources/shaders/core_2d/as_2d_layout.glsl
@@ -3,14 +3,6 @@
 #define AS_MAX_GPU_SCREEN_OBJECT_CUSTOM_INFO_SIZE 16 // has to match the cpu side
 #define AS_MAX_GPU_SCREEN_OBJECT_CUSTOM_DATA_SIZE 256 // has to match the cpu side
 
-
-	//mat4 data;
-	// position	x[0][0] y[0][1]
-	// rotation	x[0][2] y[0][3]
-	// extent	x[1][0] y[1][1]
-	// [2][...]	empty
-	// [3][...]	empty
-
 layout(binding = 0) uniform uniform_buffer_screen_object 
 {
 	uint custom_data[AS_MAX_GPU_SCREEN_OBJECT_CUSTOM_DATA_SIZE];
