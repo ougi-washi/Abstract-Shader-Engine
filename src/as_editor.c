fc027000fcfcf735fd769fab0ef950475b169b85
オウギワシ <jedfakhfekh@gmail.com>
2024-04-14 19:59:01 +0900

    Add base sky implementation for editor

--- src/as_editor.c
+++ src/as_editor.c
@@ -8,6 +8,7 @@
 #define AS_PATH_EDITOR_SAND_VERT_SHADER 		"../resources/shaders/editor/editor_sand_vert.glsl"
 #define AS_PATH_EDITOR_SAND_FRAG_SHADER 		"../resources/shaders/editor/editor_sand_frag.glsl"
 #define AS_PATH_EDITOR_MOUNTAIN_FRAG_SHADER 	"../resources/shaders/editor/editor_mountain_frag.glsl"
+#define AS_PATH_EDITOR_SKY_FRAG_SHADER 			"../resources/shaders/editor/editor_sky_frag.glsl"
 
 #define AS_PATH_EDITOR_SAND_TEX_SHADER		"../resources/textures/editor/rawpixel_sand_mask.jpg"
 
@@ -40,7 +41,7 @@ void as_editor_set_default_scene()
 	as_scene* editor_scene = as_scene_create(as_engine_get_render(), AS_PATH_EDITOR_DEFAULT_SCENE);
 	as_engine_set_scene(editor_scene);
 
-	as_camera* camera = as_camera_create(AS_VEC_PTR(as_vec3, -30.f, -16.f, 0.f), AS_VEC_PTR(as_vec3, 0.f, 0.f, 10.f));
+	as_camera* camera = as_camera_create(AS_VEC_PTR(as_vec3, -36.f, -4.f, 2.f), AS_VEC_PTR(as_vec3, 0.f, 0.f, 10.f));
 	as_camera_set_view(camera, AS_CAMERA_FREE);
 
 #pragma region SCENE
@@ -55,8 +56,13 @@ void as_editor_set_default_scene()
 
 	as_shader* shader_mountain = as_shader_create(AS_PATH_EDITOR_DEFAULT_VERT_SHADER, AS_PATH_EDITOR_MOUNTAIN_FRAG_SHADER);
 	as_object* object_mountain = as_object_create(shape_sphere, shader_mountain);
-	as_object_set_scale(object_mountain, AS_VEC_PTR(as_vec3, 100., 100., 100.));
-	as_object_set_translation(object_mountain, AS_VEC_PTR(as_vec3, 100.));
+	as_object_set_scale(object_mountain, AS_VEC_PTR(as_vec3, 40., 40., 40.));
+	as_object_set_translation(object_mountain, AS_VEC_PTR(as_vec3, 10., 0., -1.));
+
+	as_shader* shader_sky = as_shader_create(AS_PATH_EDITOR_DEFAULT_VERT_SHADER, AS_PATH_EDITOR_SKY_FRAG_SHADER);
+	as_object* object_sky = as_object_create(shape_sphere, shader_sky);
+	as_object_set_scale(object_sky, AS_VEC_PTR(as_vec3, 200., 200., 200.));
+	as_object_set_translation(object_sky, AS_VEC_PTR(as_vec3, 180., 0., -60.));
 #pragma endregion SCENE
 
 #pragma region UI
